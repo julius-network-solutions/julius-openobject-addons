f3dc6d7df32ea2a9f92fbdd65322c2ebd522add1
Yvan PATRY <yvan@julius.fr>
2013-07-01 11:58:08 +0200

    [ADD] Add two new module

---
+++ sale_on_company/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+{
+    "name" : "Sale on company",
+    "version" : "1.0",
+    "author" : "Julius Network Solutions",
+    "website" : "http://julius.fr",
+    "category" : "Sales Management",
+    "depends" : [
+        'sale',
+    ],
+    "description": """
+    Sale order are now only available for Partner Company
+    """,
+    "demo" : [],
+    "data" : [
+        'sale_view.xml',
+    ],
+    'installable' : True,
+    'active' : False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
