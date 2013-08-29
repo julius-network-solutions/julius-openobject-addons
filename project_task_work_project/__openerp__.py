204a78211768f3cbffe23de0eaedd6ca5a60c221
Yvan PATRY <yvan@julius.fr>
2013-08-29 11:35:19 +0200

    [IMP] Add new module

---
+++ project_task_work_project/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+{
+    "name" : "Task Work add project",
+    "version": "1.0",
+    "author" : "Julius Network Solutions",
+    "website" : "http://www.julius.fr",
+    "category" : "Generic Modules/Projects & Services",
+    "depends" : ["project",
+                 "project_task_work_calendar_view",
+                 ],
+    "description": """Add related project in task work views""",
+    "init_xml" : [],
+    "demo_xml" : [],
+    "update_xml": [
+        "project_task_work_view.xml", 
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
