a96d036224dc6ae20b5df31b7f62ab5f99519d1f
Mathieu VATEL <mathieu@julius.fr>
2013-09-13 17:04:00 +0200

    [IMP] add a module which is blocking the moves if the sale order is waiting for the paiment

---
+++ sale_stock_block_without_payment/sale.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+from openerp.osv import fields, orm
+from openerp import netsvc
+from openerp.tools.translate import _
+
+class sale_order(orm.Model):
+    _inherit = "sale.order"
+
+    _columns = {
+        'block_without_payment': fields.boolean(
+            'Block order Without Payment', readonly=True,
+            states={
+                    'draft': [('readonly', False)],
+                    'sent': [('readonly', False)],
+                    }),
+        'marked_as_paid': fields.boolean('Marked as Paid', readonly=True),
+    }
+    
+    _defaults = {
+        'block_without_payment': False,
+        'marked_as_paid': False,
+    }
+
+    def set_marked_as_paid(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        return self.write(cr, uid, ids, {
+            'marked_as_paid': True,
+            }, context=context)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
