70df62a841cb56b2243c4e60bbd564099b967995
Yvan PATRY <yvan@julius.fr>
2013-10-01 17:53:17 +0200

    [ADD] New module product_partner_code_on_sale allow to see the product partner code on sale order line

---
+++ product_partner_code_on_sale/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+{
+    "name" : "Product Partner code on sale",
+    "version" : "1.0",
+    "author" : "Julius Network Solutions",
+    "website" : "http://julius.fr",
+    "category" : "Sales Management",
+    "depends" : [
+        'base',
+        'product',
+        'sale',
+        'product_partner_code'
+    ],
+    "description": """
+    Add a new object which allow to define partner codes for products
+    """,
+    "demo" : [],
+    "data" : [
+        'sale_view.xml',
+    ],
+    'installable' : True,
+    'active' : False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ product_partner_code_on_sale/sale.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+from openerp.osv import fields, orm
+from openerp.tools.translate import _
+
+class sale_order_line(orm.Model):
+    _inherit = 'sale.order.line'
+    
+    def _get_product_partner_code(self, cr, uid, ids, name, args, context=None):
+        if context is None:
+            context = {}
+        res = {}
+        product_partner_code_obj = self.pool.get('product.partner.code')
+        for line in self.browse(cr, uid, ids, context=context):
+            res[line.id] = ''
+            partner_id = line.order_id.partner_id
+            if line.product_id and partner_id:
+                code_ids = product_partner_code_obj.search(cr, uid, [
+                    ('product_id', '=', line.product_id.id),
+                    ('partner_id', '=', partner_id.id),
+                    ], limit=1, context=context)
+                if code_ids:
+                    code = product_partner_code_obj.browse(cr, uid, code_ids[0], context=context).code or ''
+                    res[line.id] = code
+        return res
+    
+    _columns = {
+        'product_partner_code': fields.function(_get_product_partner_code,
+            string='Product Partner Code', type='char', size=64),
+    }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
