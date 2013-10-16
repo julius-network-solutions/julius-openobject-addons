0b0dfca98c1077050ec303b49e4ad558ea062dc7
Yvan PATRY <yvan@julius.fr>
2013-10-16 11:52:10 +0200

    [FIX] Fix creation issue

--- constraint_prodlot_move/move.py
+++ constraint_prodlot_move/move.py
@@ -31,7 +31,7 @@ class stock_move(orm.Model):
                 location_id = move_data.location_id.id
                 prodlot_id = move_data.prodlot_id.id
                 current_location_id = move_data.prodlot_id.current_location_id.id
-                if current_location_id != location_id:
+                if current_location_id and current_location_id != location_id:
                     name = move_data.prodlot_id.name
                     raise osv.except_osv(_('Error'), _('The Origin of this move does not match the current position for this serial : %s') % name)
                     return False
