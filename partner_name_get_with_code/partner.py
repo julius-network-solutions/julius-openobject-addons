b593d6f3f6768ded23502b224dbc51b925350d1f
Mathieu VATEL <mathieu@julius.fr>
2013-10-28 09:18:50 +0100

    [IMP] - make possible to get the partners and child of this partner

--- partner_name_get_with_code/partner.py
+++ partner_name_get_with_code/partner.py
@@ -20,6 +20,7 @@
 #################################################################################
 from openerp.osv import fields, orm
 from openerp.tools.translate import _
+import re
 
 class res_partner(orm.Model):
     _inherit = "res.partner"
@@ -36,5 +37,31 @@ class res_partner(orm.Model):
             new_name = (record['ref'] and '[' + record['ref'] + '] - ' or '') + name
             res2.append((partner_id, new_name))
         return res2
+
+    def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args = []
+        if name:
+            ids = self.search(cr, user, [('ref','=',name)]+ args, limit=limit, context=context)
+            if not ids:
+                # Do not merge the 2 next lines into one single search, SQL search performance would be abysmal
+                # on a database with thousands of matching products, due to the huge merge+unique needed for the
+                # OR operator (and given the fact that the 'name' lookup results come from the ir.translation table
+                # Performing a quick memory merge of ids in Python will give much better performance
+                ids = set()
+                ids.update(self.search(cr, user, args + [('ref',operator,name)], limit=limit, context=context))
+                if not limit or len(ids) < limit:
+                    # we may underrun the limit because of dupes in the results, that's fine
+                    ids.update(self.search(cr, user, args + [('name',operator,name)], limit=(limit and (limit-len(ids)) or False) , context=context))
+                ids = list(ids)
+            if not ids:
+                ptrn = re.compile('(\[(.*?)\])')
+                res = ptrn.search(name)
+                if res:
+                    ids = self.search(cr, user, [('ref','=', res.group(2))] + args, limit=limit, context=context)
+        else:
+            ids = self.search(cr, user, args, limit=limit, context=context)
+        result = self.name_get(cr, user, ids, context=context)
+        return result
     
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
