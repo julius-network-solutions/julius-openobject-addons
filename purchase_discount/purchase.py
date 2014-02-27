1657bdacd55784a82fc9a58d72347c21ba444e5f
Yvan PATRY <yvan@julius.fr>
2014-02-27 15:29:28 +0100

    [ADD] new module Purchase Discount

---
+++ purchase_discount/purchase.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import time
+from openerp.osv import fields, orm
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP, float_compare
+import openerp.addons.decimal_precision as dp
+from openerp import netsvc
+
+class purchase_order(orm.Model):
+    _inherit = 'purchase.order'
+    def _amount_all_discount(self, cr, uid, ids, field_name, arg, context=None):
+        res = {}
+        cur_obj=self.pool.get('res.currency')
+        for order in self.browse(cr, uid, ids, context=context):
+            res[order.id] = {
+                'amount_untaxed': 0.0,
+                'amount_tax': 0.0,
+                'amount_total': 0.0,
+            }
+            val = val1 = 0.0
+            cur = order.pricelist_id.currency_id
+            for line in order.order_line:
+               val1 += line.price_subtotal
+               price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+               for c in self.pool.get('account.tax').compute_all(cr, uid, line.taxes_id, price, line.product_qty, line.product_id, order.partner_id)['taxes']:
+                    val += c.get('amount', 0.0)
+            res[order.id]['amount_tax']=cur_obj.round(cr, uid, cur, val)
+            res[order.id]['amount_untaxed']=cur_obj.round(cr, uid, cur, val1)
+            res[order.id]['amount_total']=res[order.id]['amount_untaxed'] + res[order.id]['amount_tax']
+        return res
+    
+    def _get_order_discount(self, cr, uid, ids, context=None):
+        result = {}
+        for line in self.pool.get('purchase.order.line').browse(cr, uid, ids, context=context):
+            result[line.order_id.id] = True
+        return result.keys()
+    
+    _columns = { 
+        'amount_untaxed': fields.function(_amount_all_discount, digits_compute= dp.get_precision('Account'), string='Untaxed Amount',
+            store={
+                'purchase.order.line': (_get_order_discount, None, 10),
+            }, multi="sums", help="The amount without tax", track_visibility='always'),
+        'amount_tax': fields.function(_amount_all_discount, digits_compute= dp.get_precision('Account'), string='Taxes',
+            store={
+                'purchase.order.line': (_get_order_discount, None, 10),
+            }, multi="sums", help="The tax amount"),
+        'amount_total': fields.function(_amount_all_discount, digits_compute= dp.get_precision('Account'), string='Total',
+            store={
+                'purchase.order.line': (_get_order_discount, None, 10),
+            }, multi="sums",help="The total amount"),
+    }
+    
+    def _prepare_inv_line(self, cr, uid, account_id, order_line, context=None):
+        res = super(purchase_order, self)._prepare_inv_line(cr, uid, account_id=account_id, order_line=order_line,context=None)
+        if order_line:
+            res['discount'] = order_line.discount
+        return res
+
+class purchase_order_line(orm.Model):
+    _inherit = 'purchase.order.line'
+    
+    def _amount_line_discount(self, cr, uid, ids, field_name, arg, context=None):
+        tax_obj = self.pool.get('account.tax')
+        cur_obj = self.pool.get('res.currency')
+        res = {}
+        if context is None:
+            context = {}
+        for line in self.browse(cr, uid, ids, context=context):
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = tax_obj.compute_all(cr, uid, line.taxes_id, price, line.product_qty, line.product_id, line.order_id.partner_id)
+            cur = line.order_id.pricelist_id.currency_id
+            res[line.id] = cur_obj.round(cr, uid, cur, taxes['total'])
+        return res
+    
+    
+    _columns = {
+        'discount': fields.float('Discount (%)', digits_compute= dp.get_precision('Discount')),
+        'price_subtotal': fields.function(_amount_line_discount, string='Subtotal', digits_compute= dp.get_precision('Account')),
+    }
+    
+    _defaults = {
+        'discount': 0.0,
+    }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ purchase_discount/stock.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+from openerp.osv import fields, orm
+from openerp.tools.translate import _
+
+class stock_picking(orm.Model):
+    _inherit = "stock.picking"
+    def _prepare_invoice_line(self, cr, uid, group, picking, move_line, invoice_id,
+        invoice_vals, context=None):
+        res = super(stock_picking, self)._prepare_invoice_line(cr, uid, group, picking=picking,
+            move_line=move_line, invoice_id=invoice_id,invoice_vals=invoice_vals, context=None)
+        if move_line.purchase_line_id:
+            res['discount'] = move_line.purchase_line_id.discount
+        return res
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
