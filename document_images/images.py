0e81fa6591b620a3cf43df73cdfb3899e6357378
Mathieu VATEL <mathieu@julius.fr>
2014-03-10 15:08:03 +0100

    [IMP] - add the possibility to get the image if URL or there's no filestore defined

--- document_images/images.py
+++ document_images/images.py
@@ -21,13 +21,53 @@
 
 from openerp.osv import orm, fields
 from openerp.tools.translate import _
-
 import logging
-import imghdr
-
 _logger = logging.getLogger(__name__)
 
+import imghdr
+import mimetypes
+import urllib
+import urllib2
+import base64
+try:
+    from PIL import Image
+except:
+    _logger.warning("ERROR IMPORTING PIL, if not installed, please install it:"
+    " get it here: https://pypi.python.org/pypi/PIL")
+import io
+
+def is_url_image(url):    
+    mimetype,encoding = mimetypes.guess_type(url)
+    return (mimetype and mimetype.startswith('image'))
+
+def check_url(url):
+    """Returns True if the url returns a response code between 200-300,
+       otherwise return False.
+    """
+    try:
+        headers={
+            "Range": "bytes=0-10",
+            "User-Agent": "MyTestAgent",
+            "Accept":"*/*"
+        }
+
+        req = urllib2.Request(url, headers=headers)
+        response = urllib2.urlopen(req)
+        return response.code in range(200, 209)
+    except Exception, ex:
+        return False
+
+def is_image_and_ready(url):
+    return check_url(url) and is_url_image(url) or False
+
+def get_image_data_from_url(url):
+    u = urllib.urlopen(url)
+    raw_data = u.read()
+    u.close()
+    return base64.encodestring(raw_data)
+
 TYPE_IMAGES = ['png', 'jpg', 'gif', 'bmp', 'svg', 'jpeg']
+FORMAT_IMAGES = ['PNG', 'JPG', 'GIF', 'BMP', 'SVG', 'JPEG']
 
 class ir_attachment(orm.Model):
     _inherit = 'ir.attachment'
@@ -41,16 +81,33 @@ class ir_attachment(orm.Model):
         location = parameter_obj.get_param(cr, uid, 'ir_attachment.location')
         for document in self.browse(cr, uid, ids, context=context):
             res[document.id] = False
-            if document.type == 'binary' and location and document.store_fname:
-                full_path = self._full_path(cr, uid,
-                                            location, document.store_fname)
+            if document.type == 'url':
                 is_image = False
                 try:
-                    type = imghdr.what(full_path)
-                    if type in TYPE_IMAGES:
-                        is_image = True
+                    is_image = is_image_and_ready(document.url)
                 except IOError:
                     _logger.error("_read_file reading %s",full_path)
+                res[document.id] = is_image
+            elif document.type == 'binary':
+                if location and document.store_fname:
+                    full_path = self._full_path(cr, uid,
+                                                location, document.store_fname)
+                    is_image = False
+                    try:
+                        type = imghdr.what(full_path)
+                        if type in TYPE_IMAGES:
+                            is_image = True
+                    except IOError:
+                        _logger.error("_read_file reading %s",full_path)
+                if not location:
+                    is_image = False
+                    try:
+                        image_stream = io.BytesIO(document.datas.decode('base64'))
+                        image = Image.open(image_stream)
+                        if image.format in FORMAT_IMAGES:
+                            is_image = True
+                    except IOError:
+                        _logger.error("_read_file reading %s", document.name)
                 res[document.id] = is_image
         return res
     
@@ -60,7 +117,10 @@ class ir_attachment(orm.Model):
         for document in self.browse(cr, uid, ids, context=context):
             res[document.id] = False
             if document.is_image:
-                res[document.id] = document.datas
+                if document.type == 'binary':
+                    res[document.id] = document.datas
+                else:
+                    res[document.id] = get_image_data_from_url(document.url)
         return res
 
     _columns = {
