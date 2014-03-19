ba596a8ad8c8beff3b7d1a2ca0484300bcf4af6a
Mathieu VATEL <mathieu@julius.fr>
2014-03-19 16:51:57 +0100

    [IMP] - code cleaning

--- account_bank_statement_import/wizard/bank_statement_import.py
+++ account_bank_statement_import/wizard/bank_statement_import.py
@@ -162,14 +162,20 @@ class account_bank_statement_import(orm.TransientModel):
     }
     
     _defaults = {
-        'journal_id': lambda self, y, z, c: self._default_journal_id(y, z, c),
-        'payable_id': lambda self, y, z, c: self._default_def_payable(y, z, c),
-        'receivable_id': lambda self, y, z, c: self._default_def_receivable(y, z, c),
-        'awaiting_id': lambda self, y, z, c: self._default_def_awaiting(y, z, c),
-        'filter_id': lambda self, y, z, c: self._default_filter_id(y, z, c),
-        'statement_update': False,
-        'date_format': lambda self, y, z, c: self._default_date_format(y, z, c),
+        'journal_id': lambda self, cr, uid, context:
+            self._default_journal_id(cr, uid, context),
+        'payable_id': lambda self, cr, uid, context:
+            self._default_def_payable(cr, uid, context),
+        'receivable_id': lambda self, cr, uid, context:
+            self._default_def_receivable(cr, uid, context),
+        'awaiting_id': lambda self, cr, uid, context:
+            self._default_def_awaiting(cr, uid, context),
+        'filter_id': lambda self, cr, uid, context:
+            self._default_filter_id(cr, uid, context),
+        'date_format': lambda self, cr, uid, context:
+            self._default_date_format(cr, uid, context),
         'file_fname': '',
+        'statement_update': False,
     }
     
     def get_file(self, cr, uid, ids, recordlist, filter_id, data, context):
@@ -180,8 +186,9 @@ class account_bank_statement_import(orm.TransientModel):
         exec "from filters import " + filter_name + " as parser"
         # opening the file speficied as bank_file and read the data
         try:
+            # parse the data through the filter
             bank_statements = parser.\
-                get_data(self, cr, uid, ids, recordlist, data) # parse the data through the filter
+                get_data(self, cr, uid, ids, recordlist, data)
         except IOError:
             raise
         return bank_statements
@@ -239,7 +246,8 @@ class account_bank_statement_import(orm.TransientModel):
         voucher_line_obj = self.pool.get('account.voucher.line')
         line_obj = self.pool.get('account.move.line')
         
-        name = line['name'][:3] + '/' + line['name'][3:7] + '/' + line['name'][7:]
+        name = line['name'][:3] + '/' + \
+            line['name'][3:7] + '/' + line['name'][7:]
         rec_ids = line_obj.search(cr, uid, [
             ('name', '=', name),
             ('reconcile_id', '=', False),
@@ -247,59 +255,84 @@ class account_bank_statement_import(orm.TransientModel):
         ], limit=1, context=context)
         if rec_ids:
             result = voucher_obj.onchange_partner_id(cr, uid, [],
-                partner_id=line['partner_id'], journal_id=statement['journal_id'],
-                price=abs(line['amount']), currency_id = journal.company_id.currency_id.id,
-                ttype=(line['amount'] < 0 and 'payment' or 'receipt'), context=context)
+                partner_id=line['partner_id'],
+                journal_id=statement['journal_id'],
+                price=abs(line['amount']),
+                currency_id =journal.company_id.currency_id.id,
+                ttype=(line['amount'] < 0 and 'payment' or 'receipt'),
+                context=context)
             voucher_res = {
-                'type':((line.get('amount') or 0) < 0 and 'payment' or 'receipt'),
+                'type': ((line.get('amount') or 0) < 0 and \
+                         'payment' or 'receipt'),
                 'name': line.get('name') or '',
                 'journal_id': journal.id,
-                'account_id': result.get('value') and result['value'].get('account_id') or journal.default_credit_account_id.id,
+                'account_id': result.get('value') and \
+                    result['value'].get('account_id') or \
+                    journal.default_credit_account_id.id,
                 'company_id': journal.company_id.id,
                 'currency_id': journal.company_id.currency_id.id,
                 'date': line.get('date') or False,
                 'amount': abs(line.get('amount') or 0),
                 'period_id': statement.get('period_id') or period,
             }
-            voucher_id = voucher_obj.create(cr, uid, voucher_res, context=context)
+            voucher_id = voucher_obj.\
+                create(cr, uid, voucher_res, context=context)
             context.update({'move_line_ids': rec_ids})
             voucher_line_dict =  False
             if result['value']['line_ids']:
                 for line_dict in result['value']['line_ids']:
-                    move_line = line_obj.browse(cr, uid, line_dict['move_line_id'], context)
+                    move_line = line_obj.\
+                        browse(cr, uid, line_dict['move_line_id'], context)
                     if line.move_id.id == move_line.move_id.id:
                         voucher_line_dict = line_dict
 
             if voucher_line_dict:
-                voucher_line_dict.update({'voucher_id':voucher_id})
-                voucher_line_obj.create(cr, uid, voucher_line_dict, context=context)
+                voucher_line_dict.update({'voucher_id': voucher_id})
+                line_id = voucher_line_obj.\
+                    create(cr, uid, voucher_line_dict, context=context)
             mv = line_obj.browse(cr, uid, rec_ids[0], context=context)
             if mv.partner_id:
                 line['partner_id'] = mv.partner_id.id
                 if line['amount'] < 0:
-                    line['account_id'] = mv.partner_id.property_account_payable.id
+                    line['account_id'] = mv.partner_id.\
+                        property_account_payable.id
                 else:
-                    line['account_id'] = mv.partner_id.property_account_receivable.id
+                    line['account_id'] = mv.partner_id.\
+                        property_account_receivable.id
         return voucher_id, line
 
-    def _create_bank_statement_line(self, cr, uid, statement, journal, bk_st_id, context=None):
+    def _create_bank_statement_line(self, cr, uid,
+                                    statement, journal,
+                                    bk_st_id, context=None):
         if context is None:
             context = {}
-        bank_statement_line_obj = self.pool.get('account.bank.statement.line')
+        line_obj = self.pool.get('account.bank.statement.line')
         lines = statement.get('bank_statement_line') or {}
         str_not1 = ''
         for key in lines.keys():
             line = lines[key]
             if not line['partner_id']:
-                line['partner_id'] = journal.company_id and journal.company_id.partner_id.id or False
+                line['partner_id'] = journal.company_id and \
+                    journal.company_id.partner_id.id or False
             voucher_id = False
             if line.get('toreconcile') or False: # Fix me
-                voucher_id, line = self._voucher_create(cr, uid, statement=statement, line=line, journal=journal, context=context)
+                voucher_id, line = self.\
+                    _voucher_create(cr, uid, statement=statement,
+                                    line=line, journal=journal,
+                                    context=context)
 
             if line.has_key('contry_name') and line.has_key('cntry_number'):
-                str_not1 += "Partner name:%s \n Partner Account Number:%s \n Communication:%s \n Value Date:%s \n Entry Date:%s \n"%(line["contry_name"], line["cntry_number"], line["free_comm"]+line['extra_note'], line["val_date"][0], line["entry_date"][0])
-            line_vals = self._get_line_vals(cr, uid, line, bk_st_id, voucher_id, str_not1, context=context)
-            bank_statement_line_obj.create(cr, uid, line_vals, context=context)
+                str_not1 += "Partner name:%s \n Partner Account Number:%s \n"\
+                "Communication:%s \n Value Date:%s \n"
+                "Entry Date:%s \n" %(line["contry_name"],
+                                     line["cntry_number"],
+                                     line["free_comm"]+line['extra_note'],
+                                     line["val_date"][0],
+                                     line["entry_date"][0])
+            line_vals = self.\
+                _get_line_vals(cr, uid, line, bk_st_id,
+                               voucher_id, str_not1, context=context)
+            line_id = line_obj.create(cr, uid, line_vals, context=context)
         return str_not1
 
     def file_parsing(self, cr, uid, ids, context=None):
@@ -332,16 +365,20 @@ class account_bank_statement_import(orm.TransientModel):
         recordlist = base64.decodestring(unicode(file_data, 'utf-8')).split('\n')
         recordlist.pop()
         
-        bank_statements = self.get_file(cr, uid, ids, recordlist, filter_id, data, context=context)
+        bank_statements = self.\
+            get_file(cr, uid, ids, recordlist, filter_id, data, context=context)
         statement_update = data['statement_update']
         #end for
         bkst_list = []
         for statement in bank_statements:
             statement_journal_id = statement.get('journal_id') or journal_id
-            journal = journal_obj.browse(cr, uid, statement_journal_id, context=context)
+            journal = journal_obj.\
+                browse(cr, uid, statement_journal_id, context=context)
             try:       
-                '''If the month already exist we update the statement''' 
-                '''Section to be remove if we do not want a fusion of statement anymore'''
+                """If the month already exist
+                we update the statement
+                Section to be remove
+                if we do not want a fusion of statement anymore"""
                 if statement_update:
                     bk_st_ids = bank_statement_obj.search(cr, uid, [
                         ('period_id', '=', statement.get('period_id')),
@@ -349,27 +386,35 @@ class account_bank_statement_import(orm.TransientModel):
                         ], limit=1, context=context)
                     if bk_st_ids:
                         bk_st_id = bk_st_ids[0]
-                        statement_data = bank_statement_obj.browse(cr, uid, bk_st_id, context=context)
+                        statement_data = bank_statement_obj.\
+                            browse(cr, uid, bk_st_id, context=context)
                         statement_total_amount = statement.get('total_amount') or 0
                         balance_start = statement_data.balance_start                    
-                        balance_end_real = statement_data.balance_end_real + statement_total_amount
+                        balance_end_real = statement_data.balance_end_real + \
+                            statement_total_amount
                         bank_statement_obj.write(cr, uid, [bk_st_id], {
                                 'balance_end_real': balance_end_real,
                             }, context=context)
                         bkst_list.append(bk_st_id)
-                        str_not1 = self._create_bank_statement_line(cr, uid, statement, journal, bk_st_id, context=context)
+                        str_not1 = self.\
+                            _create_bank_statement_line(cr, uid, statement,
+                                                        journal, bk_st_id,
+                                                        context=context)
                 
                 '''If the month does not exist we create a new statement'''
                 if not bank_statement_obj.search(cr, uid, [
                         ('period_id', '=', statement.get('period_id',False))
                         ], context=context) or statement_update == False:
                     if not statement.get('name',False):                    
-                        statement['name'] = seq_obj.next_by_code(cr, uid, 'account.bank.statement')
-                    previous_bank_statement_ids = bank_statement_obj.search(cr, uid, [],
-                            order=('date desc,period_id desc'), context=context)
+                        statement['name'] = seq_obj.\
+                            next_by_code(cr, uid, 'account.bank.statement')
+                    previous_bank_statement_ids = bank_statement_obj.\
+                        search(cr, uid, [],
+                        order=('date desc,period_id desc'), context=context)
                     if previous_bank_statement_ids:
-                        balance_start = bank_statement_obj.browse(cr, uid,
-                            previous_bank_statement_ids[0], context=context).balance_end_real 
+                        balance_start = bank_statement_obj.\
+                            browse(cr, uid, previous_bank_statement_ids[0],
+                                   context=context).balance_end_real
                     else:
                         balance_start = 0
                     statement_total_amount = statement.get('total_amount') or 0
@@ -384,9 +429,12 @@ class account_bank_statement_import(orm.TransientModel):
                         'name': statement.get('name') or False,
                     }, context=context)
                     bkst_list.append(bk_st_id)
-                    str_not1 = self._create_bank_statement_line(cr, uid, statement, journal, bk_st_id, context=context)
+                    str_not1 = self.\
+                        _create_bank_statement_line(cr, uid, statement,
+                                                    journal, bk_st_id,
+                                                    context=context)
 
-            except osv.except_osv, e:
+            except orm.except_orm, e:
                 cr.rollback()
                 nb_err += 1
                 err_log += '\n' + _('Application Error:') + ' ' + str(e)
@@ -402,13 +450,18 @@ class account_bank_statement_import(orm.TransientModel):
                 nb_err+=1
                 err_log += '\n' + _('Unknown Error')
                 raise
-        sumup_log = '\n' + _('Sum up:') + '\n' + _('Number of statements:') + ' ' \
-            + str(len(bkst_list)) + '\n' +  _('Number of error:') + ' ' + str(nb_err)
+        sumup_log = '\n' + _('Sum up:') + '\n' + \
+            _('Number of statements:') + ' ' \
+            + str(len(bkst_list)) + '\n' + \
+            _('Number of error:') + ' ' + str(nb_err)
         
         total_log = str_log1 + err_log + sumup_log
         self.write(cr, uid, ids, {'note': total_log}, context=context)
         context.update({'statement_ids': bkst_list})
-        model_id, res_id = mod_obj.get_object_reference(cr, uid, 'account_bank_statement_import', 'account_bank_statement_file_report_view')
+        model_id, res_id = mod_obj.\
+            get_object_reference(cr, uid,
+                                 'account_bank_statement_import',
+                                 'account_bank_statement_file_report_view')
         return {
             'name': _('Result'),
             'res_id': ids[0],
@@ -426,7 +479,10 @@ class account_bank_statement_import(orm.TransientModel):
         if context == None:
             context = {}
         mod_obj = self.pool.get('ir.model.data')
-        model_id, res_id = mod_obj.get_object_reference(cr, uid, 'account_bank_statement_import', 'action_bank_statement_imported')
+        model_id, res_id = mod_obj.\
+            get_object_reference(cr, uid,
+                                 'account_bank_statement_import',
+                                 'action_bank_statement_imported')
         
         action_obj = self.pool.get('ir.actions.act_window')
         action = action_obj.read(cr, uid, res_id, [], context=context)
