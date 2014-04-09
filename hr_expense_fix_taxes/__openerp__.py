a5572067a0f3cea97e3d492546089858e1c1196e
Mathieu VATEL <mathieu@julius.fr>
2014-04-09 16:42:10 +0200

    [IMP] - make this module installable

--- hr_expense_fix_taxes/__openerp__.py
+++ hr_expense_fix_taxes/__openerp__.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+###############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2013-Today Julius Network Solutions SARL <contact@julius.fr>
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
+#    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
-
+###############################################################################
 
 {
     'name': 'Expense Management (fix taxes)',
@@ -37,6 +36,7 @@ This modules fix the tax computation when generating the expenses accounting dat
     'depends': ['hr_expense'],
     'data': [],
     'test': [],
-    'installable': False,
+    'installable': True,
 }
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
