e5e10794f4e030204e2953ca12d9235525c4ce87
Mathieu VATEL <mathieu@julius.fr>
2014-04-18 15:30:40 +0200

    [IMP] migrate the module project task work calendar view

--- project_task_work_calendar_view/__openerp__.py
+++ project_task_work_calendar_view/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-#################################################################################
+###############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013 Julius Network Solutions SARL <contact@julius.fr>
+#    Copyright (C) 2013-Today Julius Network Solutions SARL <contact@julius.fr>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -17,25 +17,31 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#################################################################################
+###############################################################################
 
 {
-    "name" : "Task Work Calendar View",
+    "name" : "Task work calendar view",
+    "name" : "Calendar view for project work input",
     "version": "1.0",
-    "author" : "Julius Network Solutions",
-    "website" : "http://www.julius.fr",
-    "category" : "Projet",
-    "depends" : ["project"],
-    "description": """Project task work views with a menu.
-Adds a calendar view for the module project and a note summarizing the task work performed for the task selected.
-You can manage your projects from the calendar view.""",
-    "init_xml" : [],
-    "demo_xml" : [],
-    "update_xml": [
+    "author": "Julius Network Solutions",
+    "website": "http://www.julius.fr",
+    "contributors": "Mathieu Vatel <mathieu@julius.fr>",
+    "category": "Project Management",
+    "depends": ["project"],
+    "description": """
+Project task work views with a menu
+===================================
+
+Adds a calendar view for the module project and a note summarizing the task
+work performed for the task selected.
+You can manage your projects from the calendar view.
+""",
+    "demo" : [],
+    "data": [
         "project_task_work_view.xml", 
     ],
-    'demo_xml': [],
-    'installable': False,
+    'installable': True,
     'active': False,
 }
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
