819cadca290adcac52c13d7ee1347794857511bd
Mathieu VATEL <mathieu@julius.fr>
2014-04-18 13:26:04 +0200

    [IMP] - migrate the module account_invoice_line_sum

--- account_invoice_line_sum/__openerp__.py
+++ account_invoice_line_sum/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-#################################################################################
+###############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2013 Julius Network Solutions SARL <contact@julius.fr>
+#    Copyright (C) 2013-Today Julius Network Solutions SARL <contact@julius.fr>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -17,30 +17,30 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#################################################################################
+###############################################################################
 
 {
     "name" : "Sum on invoice line",
+    "summary" : "Sum of quantities of lines inside invoices",
     "version" : "1.0",
     "author" : "Julius Network Solutions",
     "category" : "Accounting & Finance",
-    "description" : """Presentation:
+    "contributors": "Mathieu Vatel <mathieu@julius.fr>",
+    "description" : """
+Presentation:
+=============
 
-This module add a sum of invoice line in customer and supplier invoice
-
+This module add a sum of quantities of invoice line inside customer and supplier invoice.
 """,
     "website" : "http://www.julius.fr",
     "depends" : [
         "account",
     ],
-    "init_xml" : [],
-    "demo_xml" : [],
-    "images" : [],
-    "update_xml" : [
+    "demo" : [],
+    "data" : [
         "account_view.xml",
     ],
-    'test': [],
-    'installable': False,
+    'installable': True,
     'active': False,
 }
 
