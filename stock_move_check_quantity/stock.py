915557e6f800709a92455d32a30ef131a452ad0b
pierre@julius.fr <>
2014-04-28 15:50:28 +0200

    update

--- stock_move_check_quantity/stock.py
+++ stock_move_check_quantity/stock.py
@@ -26,57 +26,14 @@ from openerp.tools.translate import _
 class stock_move(orm.Model):
     _inherit = 'stock.move'
     
-    def onchange_lot_id(self, cr, uid, ids, prodlot_id=False, product_qty=False,
-                        loc_id=False, product_id=False, uom_id=False, context=None):
-        """ On change of production lot gives a warning message.
-        @param prodlot_id: Changed production lot id
-        @param product_qty: Quantity of product
-        @param loc_id: Location id
-        @param product_id: Product id
-        @return: Warning message
-        """
-        if not prodlot_id or not loc_id:
-            return {}
-        ctx = context and context.copy() or {}
-        ctx['location_id'] = loc_id
-        ctx.update({'raise-exception': True})
-        uom_obj = self.pool.get('product.uom')
-        product_obj = self.pool.get('product.product')
-        product_uom = product_obj.browse(cr, uid, product_id, context=ctx).uom_id
-        prodlot = self.pool.get('stock.production.lot').browse(cr, uid, prodlot_id, context=ctx)
-        location = self.pool.get('stock.location').browse(cr, uid, loc_id, context=ctx)
-        uom = uom_obj.browse(cr, uid, uom_id, context=ctx)
-        amount_actual = uom_obj._compute_qty_obj(cr, uid, product_uom, prodlot.stock_available, uom, context=ctx)
-        if (location.usage == 'internal') and (product_qty > (amount_actual or 0.0)):
-            raise orm.except_orm(_('Error'), _('Insufficient Stock for Serial Number ! , You are moving  %s (%s) but only %s (%s) available for this serial number.') % (product_qty, uom.name, amount_actual, uom.name))
-        return True
-    
     def _check_quantity(self, cr, uid, ids, context=None):
         for move_data in self.browse(cr, uid, ids, context=context):
-            if move_data.product_id.id and move_data.location_id.usage == 'internal' and move_data.location_id and move_data.state not in ('draft','confirmed','waiting','done','cancel'):
-                if not move_data.prodlot_id.id:
-                    qty = move_data.product_qty
-                    qty_on_hand = move_data.product_id.qty_available
-                    res = qty_on_hand - qty
-                    if res < 0:
-                        qty_on_hand_real = qty_on_hand
-                        name = move_data.product_id.name
-                        raise orm.except_orm(_('Error'), _('The Product %s has not enough quantity on hand (%s)') % (name,qty_on_hand_real))
-                        return False
-                else:
-                    qty = move_data.product_qty
-                    qty_on_hand = move_data.prodlot_id.stock_available
-                    res = qty_on_hand - qty
-                    if res < 0:
-                        name = move_data.prodlot_id.name
-                        raise orm.except_orm(_('Error'), _('The Prodlot %s has not enough quantity on hand (%s)') % (name,qty_on_hand))
-                        return False
+            if move_data.prodlot_id.id and move_data.product_qty != 1:
+                raise orm.except_orm(_('Error'), _('Serial move must have a qty equal to 1'))
+                return False
         return True
     
-    
     _constraints = [
-        (_check_quantity, 
-         'The Origin of this move does not match the current position of this serial',
-         ['prodlot_id'])
+        (_check_quantity, 'Serial move must have a qty equal to 1',['prodlot_id']),
     ]
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
