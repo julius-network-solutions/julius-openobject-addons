e1fe6c2783c62c007c99d932f2474f72f3a8b4f7
Ondřej Kuzník <ondrej.kuznik@credativ.co.uk>
2015-04-09 18:58:45 +0100

    [IMP] Port product_tags to v8 and enable hierarchy

--- product_tags/product.py
+++ product_tags/product.py
@@ -19,20 +19,54 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #################################################################################
-from openerp.osv import fields, orm
-from openerp.tools.translate import _
+from openerp import models, fields, api, _
 
-class product_tag(orm.Model):
+class product_tag(models.Model):
+    _description = 'Product Tags'
     _name = "product.tag"
-    _columns = {
-        'name': fields.char('Tag Name', required=True, size=64),
-    }
 
-class product_product(orm.Model):
-    _inherit = "product.product"
-    
-    _columns = {
-        'tag_ids' : fields.many2many('product.tag', 'product_product_tag_rel', 'tag_id', 'product_id', 'Tags'),
-    }
-    
+    # this should be the default according to the documentation, but no such
+    # thing is done in the actual implementation
+    _rec_name = 'display_name'
+
+    name = fields.Char('Tag Name', required=True, translate=True)
+    display_name = fields.Char('Full Name', compute='_compute_display_name')
+    active = fields.Boolean(help='The active field allows you to hide the tag without removing it.', default=True)
+    parent = fields.Many2one(string='Parent Tag', comodel_name='product.tag', select=True, ondelete='cascade')
+    child_ids = fields.One2many(string='Child Tags', comodel_name='product.tag', inverse_name='parent')
+    parent_left = fields.Integer('Left Parent', select=True)
+    parent_right = fields.Integer('Right Parent', select=True)
+
+    _parent_store = True
+    _parent_order = 'name'
+    _order = 'parent_left'
+
+    @api.one
+    @api.depends('name', 'parent.name')
+    def _compute_display_name(self):
+        """ Return the tags' display name, including their direct parent. """
+        if self.parent:
+            self.display_name = self.parent.display_name + ' / ' + self.name
+        else:
+            self.display_name = self.name
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        args = args or []
+        if name:
+            # Be sure name_search is symetric to name_get
+            name = name.split(' / ')[-1]
+            args = [('name', operator, name)] + args
+        tags = self.search(args, limit=limit)
+        return tags.name_get()
+
+class product_template(models.Model):
+    _inherit = "product.template"
+
+    tag_ids = fields.Many2many(string='Tags',
+                               comodel_name='product.tag',
+                               relation='product_product_tag_rel',
+                               column1='tag_id',
+                               column2='product_id')
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
