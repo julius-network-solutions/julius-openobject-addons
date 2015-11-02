1386b882c965eb0358a243cc5ae893008c436d6b
Mathieu VATEL <mathieu@julius.fr>
2015-11-02 11:19:54 +0100

    [IMP] add the reference link + callback to the thanks page

--- website_sms_authentication/models/sms_authentication.py
+++ website_sms_authentication/models/sms_authentication.py
@@ -29,6 +29,12 @@ class sms_authentication(models.Model):
     _description = 'SMS authentication'
 
     @api.model
+    def _select_objects(self):
+        model_pool = self.env['ir.model']
+        models = model_pool.search([], limit=None)
+        return [(r.model, r.name) for r in models] + [('', '')]
+
+    @api.model
     def _generate_code(self, number=6):
         """
         This method will generate a code.
@@ -51,6 +57,18 @@ class sms_authentication(models.Model):
     gateway_id = fields.Many2one('sms.smsclient',
                                  'SMS Gateway', readonly=True,
                                  states={'draft': [('readonly', False)]})
+    res_model = fields.Char('Res model')
+    res_id = fields.Integer('Res id')
+    link_id = fields.Reference(string='Link', selection=_select_objects,
+                               compute='_get_reference_link')
+
+    @api.one
+    def _get_reference_link(self):
+        link_id = False
+        if self.res_model and self.res_id:
+            link_id = "%s,%s" % (self.res_model, self.res_id)
+        self.link_id = link_id
+
     state = fields.Selection([
                               ('draft', 'Draft'),
                               ('sent', 'Sent'),
@@ -59,21 +77,28 @@ class sms_authentication(models.Model):
                               ('cancel', 'Cancel'),
                               ], 'State', readonly=True, default='draft')
 
-    @api.one
+    @api.multi
     def verify_code(self, code=None):
         """
         Method checking the code send to the mobile number
         """
-        if self.validity < fields.Datetime.now():
+        assert len(self.ids) == 1, \
+            "This function should only be used for a single id at a time"
+        if self.state != 'done' and \
+            self.validity <= fields.Datetime.now():
             self.state = 'cancel'
-            return
+            return False
         if self.state in ('sent', 'not_valid'):
             state = 'not_valid'
+            result = False
             if self.name == code:
                 state = 'done'
+                result = True
             self.write({
                         'state': state,
                         })
+            return result
+        return False
 
     @api.one
     def send_code(self):
@@ -82,23 +107,27 @@ class sms_authentication(models.Model):
         """
         if self.state == 'draft':
             mobile = self.mobile
-            mobile = '0634456091'
+            mobile = '+33634456091'
             self.write({
                         'state': 'sent',
                         'validity': datetime.now() + timedelta(minutes=15),
                         })
 
-    @api.one
+    @api.multi
     def send_new_code(self):
         """
         Method sending a new code to the mobile number
         """
+        assert len(self.ids) == 1, \
+            "This function should only be used for a single id at a time"
         default = {
                    'name': self._generate_code(),
                    'validity': False,
                    'state': 'draft',
                    }
         new = self.copy(default=default)
+        new.send_code()
         self.state = 'cancel'
+        return new
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
