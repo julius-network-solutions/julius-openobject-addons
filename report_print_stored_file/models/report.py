a11266b9f43d7dbca7494a58d74b55456edd15ff
Mathieu VATEL <mathieu@julius.fr>
2016-01-07 12:20:07 +0100

    [FIX] error if installed, just keep the native way to print reports

--- report_print_stored_file/models/report.py
+++ report_print_stored_file/models/report.py
@@ -99,6 +99,11 @@ class Report(models.Model):
                               'file %s' % temporary_file)
         return result
 
+    @api.v8
+    def get_pdf(self, records, report_name, html=None, data=None):
+        return self._model.get_pdf(self._cr, self._uid, records.ids, report_name,
+                                   html=html, data=data, context=self._context)
+
     def _merge_files_pdf(self, documents):
         """Merge PDF files into one.
 
