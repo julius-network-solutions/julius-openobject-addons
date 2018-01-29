2ad2ad67a5b2ee93f5cbeedfe2dfacc1f31abdfc
Yvan Patry <yvan@julius.fr>
2018-01-29 16:06:46 +0100

    [ADD] New module stock inventory date

---
+++ stock_inventory_date/stock.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Julius Network Solutions SARL <contact@julius.fr>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#################################################################################
+
+from datetime import date, datetime
+from dateutil import relativedelta
+import json
+import time
+
+from openerp.osv import fields, osv
+from openerp.tools.float_utils import float_compare, float_round
+from openerp.tools.translate import _
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
+from twisted.test.test_amp import Hello
+
+class stock_inventory(osv.osv):
+    _inherit = "stock.inventory"
+    
+    def prepare_inventory(self, cr, uid, ids, context=None):
+        inventory_line_obj = self.pool.get('stock.inventory.line')
+        for inventory in self.browse(cr, uid, ids, context=context):
+            # If there are inventory lines already (e.g. from import), respect those and set their theoretical qty
+            line_ids = [line.id for line in inventory.line_ids]
+            if not line_ids and inventory.filter != 'partial':
+                #compute the inventory lines and create them
+                vals = self._get_inventory_lines(cr, uid, inventory, context=context)
+                for product_line in vals:
+                    inventory_line_obj.create(cr, uid, product_line, context=context)
+            if not inventory.date:
+                self.write(cr, uid, ids, {'date': time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)})
+        return self.write(cr, uid, ids, {'state': 'confirm'})
+    
+    _columns = {
+        'date': fields.datetime('Inventory Date', readonly=False,required=True, help="The date that will be used for the stock level check of the products and the validation of the stock move related to this inventory."),
+    }
+    
+class stock_inventory_line(osv.osv):
+    _inherit = "stock.inventory.line"
+    
+    def _get_quants(self, cr, uid, line, context=None):
+        quant_obj = self.pool["stock.quant"]
+        dom = [('company_id', '=', line.company_id.id), ('location_id', '=', line.location_id.id), ('lot_id', '=', line.prod_lot_id.id),
+                        ('product_id','=', line.product_id.id), ('owner_id', '=', line.partner_id.id), ('package_id', '=', line.package_id.id), ('in_date','<=',  line.inventory_id.date)]
+        quants = quant_obj.search(cr, uid, dom, context=context)
+        return quants
+    
+    
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
